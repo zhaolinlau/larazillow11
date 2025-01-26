ce39f534bb3e6e033125767bcb784ef7962957eb
Zhaolin Lau <zhaolinlau@gmail.com>
2025-01-26 21:14:49 +0800

    Extracting Tailwind Styles Using @apply Directive

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -20,8 +20,7 @@ const flashSuccess = computed(() => {
 					<Link :href="route('listing.index')">LaraZillow</Link>
 				</div>
 				<div class="text-lg">
-					<Link :href="route('listing.create')"
-						class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing
+					<Link :href="route('listing.create')" class="btn-primary">+ New Listing
 					</Link>
 				</div>
 			</nav>
--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -19,80 +19,71 @@ const create = () => form.post(route('listing.store'))
 	<form @submit.prevent="create">
 		<div class="grid grid-cols-6 gap-4">
 			<div class="col-span-2">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">Beds</label>
-				<input v-model.number="form.beds" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">Beds</label>
+				<input v-model.number="form.beds" type="text" class="input" />
 				<div v-if="form.errors.beds">
 					{{ form.errors.beds }}
 				</div>
 			</div>
 
 			<div class="col-span-2">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">Baths</label>
-				<input v-model.number="form.baths" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">Baths</label>
+				<input v-model.number="form.baths" type="text" class="input" />
 				<div v-if="form.errors.baths">
 					{{ form.errors.baths }}
 				</div>
 			</div>
 
 			<div class="col-span-2">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">Area</label>
-				<input v-model.number="form.area" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">Area</label>
+				<input v-model.number="form.area" type="text" class="input" />
 				<div v-if="form.errors.area">
 					{{ form.errors.area }}
 				</div>
 			</div>
 
 			<div class="col-span-4">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">City</label>
-				<input v-model="form.city" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">City</label>
+				<input v-model="form.city" type="text" class="input" />
 				<div v-if="form.errors.city">
 					{{ form.errors.city }}
 				</div>
 			</div>
 
 			<div class="col-span-2">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">Post Code</label>
-				<input v-model="form.code" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">Post Code</label>
+				<input v-model="form.code" type="text" class="input" />
 				<div v-if="form.errors.code">
 					{{ form.errors.code }}
 				</div>
 			</div>
 
 			<div class="col-span-4">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">Street</label>
-				<input v-model="form.street" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">Street</label>
+				<input v-model="form.street" type="text" class="input" />
 				<div v-if="form.errors.street">
 					{{ form.errors.street }}
 				</div>
 			</div>
 
 			<div class="col-span-2">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">Street Nr</label>
-				<input v-model.number="form.street_nr" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">Street Nr</label>
+				<input v-model.number="form.street_nr" type="text" class="input" />
 				<div v-if="form.errors.street_nr">
 					{{ form.errors.street_nr }}
 				</div>
 			</div>
 
 			<div class="col-span-6">
-				<label class="block mb-1 text-gray-500 dark:text-gray-300 font font-medium">Price</label>
-				<input v-model.number="form.price" type="text"
-					class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+				<label class="label">Price</label>
+				<input v-model.number="form.price" type="text" class="input" />
 				<div v-if="form.errors.price">
 					{{ form.errors.price }}
 				</div>
 			</div>
 
 			<div class="col-span-6">
-				<button type="submit"
-					class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">Create</button>
+				<button type="submit" class="btn-primary">Create</button>
 			</div>
 		</div>
 	</form>
